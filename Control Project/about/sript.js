e0aad2bdbfc2f74fbc0c05e3fd5643a690ed0425
Varenij <rumpelstilzhen1@gmail.com>
2020-10-30 20:12:33 +0200

    add react blog to portfolio

--- Control Project/about/sript.js
+++ Control Project/about/sript.js
@@ -71,40 +71,6 @@ cross.addEventListener('click', burgerFunction);
         });
     });
 
-
-$('.responsive').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
-
 //модальные окна
 let getInTouch = document.querySelector('#getInTouchBut');
 let modal = document.querySelector('.modal');
